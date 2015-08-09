a67bc13c5f1fb848f53ca8a05b1aed7e464300b0
Rahul Kumar <rahulkumar.aws@gmail.com>
2015-08-09 15:02:21 +0530

    Spark WordCount Added

---
+++ src/main/scala/SparkWordCount.scala
@@ -0,0 +1,19 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+object SparkWordCount {
+
+	def main(args: Array[String]): Unit = {
+	  
+	  val sc = new SparkContext("local","SparkWordCount")
+
+	  val wordsCounted = sc.textFile(args(0)).map(line=> line.toLowerCase)
+	  										 .flatMap(line => line.split("""\W+"""))
+	  										 .groupBy(word => word)
+	  										 .map{ case(word, group) => (word, group.size)}
+
+	  wordsCounted.saveAsTextFile(args(1))
+	  sc.stop()						
+	}
+						
+	
+}
